b3316786809ff3a9a50511bdefca0788c94fa24c
Sunnan Kamal <79265528+Skamal03@users.noreply.github.com>
2025-05-28 23:36:11 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// mongo connection
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+// Routes
+const movieRoutes = require('./routes/movies');
+app.use('/movies', movieRoutes);
+
+const streamRoutes = require('./routes/stream');
+app.use('/stream', streamRoutes);
+
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
